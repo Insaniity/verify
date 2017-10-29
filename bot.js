92f43ea2d466ad68ed5c48204de257da6ba43c1c
Insaniity <iiyolo@icloud.com>
2017-10-29 19:14:13 +0000

    gg
    gg

--- bot.js
+++ bot.js
@@ -62,6 +62,7 @@ client.on('message', (message) => {
                 } else {
                     var captcha = Math.floor(Math.random() * 9000) + 1001;
                     var floor = Math.floor(Math.random() * 10000) + 1;
+                    var jesus = Math.floor(Math.random() * 156251325125) + 125464323514;
                     let webshot = require("webshot");
                     var fontFace, fontSize, fontPosition;
                     if (floor < 5000) {
@@ -91,7 +92,7 @@ client.on('message', (message) => {
                     if (bgColor === fontColor) {
                         fontColor = colors[Math.floor(Math.random() * 4) + 1];
                     }
-                    webshot('<html><body style=\'background-image: url("http://b.reich.io/jjvoab.png");\'><h1 style="font-family:' + fontFace + '; color:' + fontColor + '; font-size:' + fontSize + 'px; position: absolute; top:' + height + ';left:' + width + '; -moz-transform: rotate(' + rotate + 'deg); -ms-transform: rotate(' + rotate + 'deg);-o-transform: rotate(' + rotate + 'deg);-webkit-transform: rotate(' + rotate + 'deg);letter-spacing: ' + letterSpacing + 'px;"><i><del>' + captcha + '</del></i></h1></body></html>', './captchas/' + floor + '.png', {
+                    webshot('<html><body style=\'background-image: url("http://b.reich.io/jjvoab.png");\'><h1 style="font-family:' + fontFace + '; color:' + fontColor + '; font-size:' + fontSize + 'px; position: absolute; top:' + height + ';left:' + width + '; -moz-transform: rotate(' + rotate + 'deg); -ms-transform: rotate(' + rotate + 'deg);-o-transform: rotate(' + rotate + 'deg);-webkit-transform: rotate(' + rotate + 'deg);letter-spacing: ' + letterSpacing + 'px;"><i><del>' + captcha + '</del></i></h1></body></html>', './captchas/HappyHalloween_' + author.username '_ID=?' + jesus + ".png", {
                         siteType: 'html',
                         screenSize: {
                             width: 500,
@@ -99,11 +100,11 @@ client.on('message', (message) => {
                         }
                     }, function (err) {
                         message.author.send("", {
-                            files: ['./captchas/' + author + floor + ".png"]
+                            files: ['./captchas/HappyHalloween_' + author.username '_ID=?' + jesus + ".png"]
                         })
                     });
                     setTimeout(function () {
-                        fs.unlinkSync("./captchas/" + author + floor + ".png");
+                        fs.unlinkSync('./captchas/HappyHalloween_' + author.username '_ID=?' + jesus + ".png");
                     }, 30000);
                     message.author.send({
                         embed: {
