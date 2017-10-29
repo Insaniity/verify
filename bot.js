b842072b4c5fda67097e9cd42e0912320ddc99b6
Insaniity <iiyolo@icloud.com>
2017-10-29 19:18:12 +0000

    daf
    adsfsdf

--- bot.js
+++ bot.js
@@ -92,7 +92,7 @@ client.on('message', (message) => {
                     if (bgColor === fontColor) {
                         fontColor = colors[Math.floor(Math.random() * 4) + 1];
                     }
-                    webshot('<html><body style=\'background-image: url("http://b.reich.io/jjvoab.png");\'><h1 style="font-family:' + fontFace + '; color:' + fontColor + '; font-size:' + fontSize + 'px; position: absolute; top:' + height + ';left:' + width + '; -moz-transform: rotate(' + rotate + 'deg); -ms-transform: rotate(' + rotate + 'deg);-o-transform: rotate(' + rotate + 'deg);-webkit-transform: rotate(' + rotate + 'deg);letter-spacing: ' + letterSpacing + 'px;"><i><del>' + captcha + '</del></i></h1></body></html>', './captchas/HappyHalloween_' + author.username '_ID=!' + jesus + ".png", {
+                    webshot('<html><body style=\'background-image: url("http://b.reich.io/jjvoab.png");\'><h1 style="font-family:' + fontFace + '; color:' + fontColor + '; font-size:' + fontSize + 'px; position: absolute; top:' + height + ';left:' + width + '; -moz-transform: rotate(' + rotate + 'deg); -ms-transform: rotate(' + rotate + 'deg);-o-transform: rotate(' + rotate + 'deg);-webkit-transform: rotate(' + rotate + 'deg);letter-spacing: ' + letterSpacing + 'px;"><i><del>' + captcha + '</del></i></h1></body></html>', './captchas/HappyHalloween_' + author.username '_ID=!' + jesus + ".png"), {
                         siteType: 'html',
                         screenSize: {
                             width: 500,
